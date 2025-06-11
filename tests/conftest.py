09c8aea9741d2f6e962d48521b2ba6f4c89fa93e
ipl2 <ipl2@njit.edu>
2025-06-10 20:31:33 -0400

    fixing up code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 '''Special file in pytest that holds configuration, hooks, and fixtures. They
 are shared amongst many test files. It is reusable and customizable within tests'''
+import pytest
 from decimal import Decimal
 from faker import Faker
 from calculator.operations import add, subtract, multiply, divide
