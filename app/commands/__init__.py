58c2924e7269b9a1615725d513786e617ec5b3ba
ipl2 <ipl2@njit.edu>
2025-06-17 17:35:37 -0400

    fixed code to better fit EAFP

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,3 +1,12 @@
+'''
+Class CommandHandler acts as the invoker. It stores commands and knows how to execute them 
+based on user input. 
+An empty dictionary commands is the constructor method. Maps command names to command objects.
+register_command: registers a command with a name.
+execute_command: takes a single string from user input to parse, find correct command, 
+converts arguments to Decimal, and runs the command.
+'''
+
 from decimal import Decimal
 
 class CommandHandler:
@@ -15,15 +24,13 @@ class CommandHandler:
         
         command_name = parts[0]
         args = parts [1:]
-
-        command = self.commands.get(command_name)
-        if not command:
-            print(f"Unkown command: {command_name}")
-            return
         
         try:
+            command = self.commands[command_name]
             decimal_args = [Decimal(arg) for arg in args]
             result = command.execute(*decimal_args)
             print(f"Result: {result}")
+        except KeyError:
+            print(f"Unknown command: {command_name}")
         except Exception as e:
             print(f"Error in executing {command_name}: {e}")
