110336e879cf17fae4e5403a8ea7e6b716b2e7b2
ipl2 <ipl2@njit.edu>
2025-06-17 16:39:53 -0400

    added class App to file

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from app.commands.add_command import AddCommand
+from app.commands.subtract_command import SubtractCommand
+from app.commands.multiply_command import MultiplyCommand
+from app.commands.divide_command import DivideCommand
+from app.command_handler import CommandHandler
+
+
+class App:
+    def __init__(self):
+        self.command_handler = CommandHandler()
+
+    def start(self):
+        self.command_handler.register_command('add', AddCommand())
+        self.command_handler.register_command('subtract', SubtractCommand())
+        self.command_handler.register_command('multiply', MultiplyCommand())
+        self.command_handler.register_command('divide', DivideCommand())
+
+        while True:
+            command_line = input("Enter command: ")
+            if command_line.lower() in ('quit'):
+                print("Goodbye!")
+                break
+            self.command_handler.execute_command(command_line)
