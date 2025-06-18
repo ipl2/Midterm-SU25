7ca237f1624322fe755cb9d9c10644c1062c69a3
ipl2 <ipl2@njit.edu>
2025-06-18 11:38:56 -0400

    added code to test REPL behavior

---
+++ tests/test_app.py
@@ -0,0 +1,26 @@
+import pytest
+from app import App
+
+def test_app_start_quit_command(capfd, monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: 'quit')
+    app = App()
+
+    with pytest.raises(SystemExit) as e:
+        app.start()
+    
+    out, err = capfd.readouterr
+    assert "Goodbye!" in out
+    assert e.type == SystemExit
+
+def test_app_start_unknown_command(capfd, monkeypatch):
+    inputs = iter(['unknown_command', 'quit'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    app = App()
+
+    with pytest.raises(SystemExit):
+        app.start()
+
+    out, err = capfd.readouterr()
+    assert "Unknown command: unknown_command" in out
+    assert "Goodbye" in out
