428c275724f66b00a190f12c0a4414a964c3b9b2
ipl2 <ipl2@njit.edu>
2025-06-18 15:26:04 -0400

    making  changes to code

--- app/commands/divide/__init__.py
+++ app/commands/divide/__init__.py
@@ -2,11 +2,13 @@ from app.commands import Command
 
 class DivideCommand(Command):
     def execute(self, *args):
+        if len(args) < 2:
+            raise ValueError("Two arguments are required.")
+        
+        if args[1] == 0:
+            raise ValueError("Cannot divide by zero")
+        
         try:
             return args[0] / args[1]
-        except IndexError:
-            raise ValueError("Two arguments are required.")
         except TypeError:
             raise ValueError("Arguments must be numbers.")
-        except ZeroDivisionError:
-            raise ValueError("Cannot divide by zero")
